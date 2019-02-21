9939adae488fbf51a8b9cba9d924c74f8759d5eb
Acemcshlaghg <Acemcshlaghg@users.noreply.github.com>
2019-02-21 08:27:03 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -3,4 +3,21 @@ public class Bishop {
   public Bishop(int player){
 		super(player, "images2/Bishop" + player);
 	}
+	public boolean isValidMove(Location from, Location to, Piece[][] b) {
+		boolean tf = true;
+		if (b[to.getRow()][to.getColumn()] == null && b[to.getRow()][to.getColumn()].getPlayer() != getPlayer())
+			tf = true;
+		else {
+			tf = false;
+		}
+		for (int row = 0; row < Math.abs(to.getRow() - from.getRow()); row++) {
+			for (int column = 0; row < Math.abs(to.getColumn() - from.getColumn()); column++) {
+				if (row == column && b[from.getRow() + row][from.getColumn() + column] != null) {
+					return false;
+				}
+			}
+		}
+		
+		return tf;
+	}
 }
