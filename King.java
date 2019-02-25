e24f324761ed76032a4edc7e6d5a4c307f031311
CarloFavaro <47420866+CarloFavaro@users.noreply.github.com>
2019-02-25 09:48:54 -0500

    Update King.java

--- King.java
+++ King.java
@@ -27,7 +27,7 @@ public class King extends Piece{
 		//makes sure king only goes one column at a time or in the same column
 		if(b[to.getRow()][to.getColumn()]==null && ((from.getColumn() == to.getColumn() && from.getRow()==(to.getRow()+1) || 
 				//makes sure king only goes one row at a time or in the same row
-				(from.getRow() == to.getRow() || from.getColumn()==(to.getColumn()+1))||
+				(from.getRow() == to.getRow() && from.getColumn()==(to.getColumn()+1))||
 				//makes sure king can go diagonal top left
 				(from.getRow() == to.getRow()-1) && (from.getColumn()==to.getColumn()-1)) ||
 				//makes sure king goes diagonal top right
