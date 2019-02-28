311cb7081f9810bfa80727f973180c1b103b4b0e
nikolasm2000 <s025888@students.lmsd.org>
2019-02-28 09:10:12 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,21 +1,29 @@
+//Class: Chess
+//Written By: Carlo Favaro
+//Date: 2/26/19
+//description: This class extends piece and contains the restrictions of the pawn's moves.
+
 import java.net.URL;
 import javax.swing.ImageIcon;
 
 public class pawn extends Piece{
 
+	private ImageIcon image;
+	private int player;
 
-  public Pawn(int player){
-		super(player, "images2/Pawn" + player);
-	 }
+
+
+
+
+	public pawn(int player) {
+		setImageIcon("images/pawn1.png");
+		this.player = player;
+	}
 
 	protected void setImageIcon(String imagePath){
 
 		ClassLoader cldr = this.getClass().getClassLoader();	
-
-
-
 		URL imageURL = cldr.getResource(imagePath);				
-
 		image = new ImageIcon(imageURL);
 
 	}
@@ -26,57 +34,55 @@ public class pawn extends Piece{
 
 		//white team
 
-		if(getPlayer()==1) {
+		
 
-			//if pawn is not in its first spot and move one spot up
+		//if pawn is not in its first spot and move one spot up
 
-			if(from.getRow()!=7 && (from.getRow()==to.getRow()+1 || 
+		if(getPlayer()==1 && from.getRow()!=7 && (from.getRow()==to.getRow()+1 || 
 
-			//pawn moving diagonal top left
+		//pawn moving diagonal top left
 
-			(from.getRow()==to.getRow()+1)&&(from.getColumn()==to.getColumn()+1))||
+		(from.getRow()==to.getRow()+1)&&(from.getColumn()==to.getColumn()+1))||
 
-			//pawn moving diagonal top right
+		//pawn moving diagonal top right
 
-			(from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1)) 
+		(from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1) ||
 
+		//pawn moves twice when in first spot
 
-
-			
-
-			if(from.getRow()==7 && from.getRow()==to.getRow()+2)
+		from.getRow()==7 && from.getRow()==to.getRow()+2) {
 
 				return true;
 
-		}
+			}
 
 
 
+		//Black Team
 
-
-		else if(getPlayer()==2) {
+		
 
 			//if pawn is not in its first spot and move one spot up
 
-			if(from.getRow()!=1 && (from.getRow()==to.getRow()-1 || 
+			if((getPlayer()==2 && from.getRow()!=1 && (from.getRow()==to.getRow()-1) || 
 
 			//pawn moving diagonal bottom left
 
-			(from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()+1))||
+			((from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()+1)))||
 
 			//pawn moving diagonal bottom right
 
-			(from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1)) 
+			((from.getRow()==to.getRow()-1)&&(from.getColumn()==to.getColumn()-1)) ||
+
+			//pawn moving twice in first move
+
+			from.getRow()==1 && from.getRow()==to.getRow()-2) {
 
 				return true;
 
-			
+			}
 
-			if(from.getRow()==1 && from.getRow()==to.getRow()-2)
-
-				return true;
-
-		}
+		
 
 
 
