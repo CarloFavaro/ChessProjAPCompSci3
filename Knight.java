847a70a7c5dcc3080c0900bc6325dd42b16d058d
nikolasm2000 <s025888@students.lmsd.org>
2019-02-28 23:42:14 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,10 +1,10 @@
 
 public class Knight extends Piece{
 	public Knight(int player) {
-		super(player, "images2/Knight" + player);
+		super(player, "images2/Knight" + player+ ".png");
 	}
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
-		if(b[to.getRow()][to.getColumn()].getPlayer() != getPlayer()) {
+		if(b[to.getRow()][to.getColumn()]== null || b[to.getRow()][to.getColumn()].getPlayer() != getPlayer()) {
 			return true;
 		}
 		else {
