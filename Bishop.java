daa818de667165366d56a05d1fa588ce94cc8281
nikolasm2000 <s025888@students.lmsd.org>
2019-02-28 23:42:42 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,18 +1,18 @@
 
 public class Bishop extends Piece {
 	  public Bishop(int player){
-			super(player, "images2/Bishop" + player);
+			super(player, "images2/Bishop" + player +".png");
 		}
 	public boolean isValidMove(Location from, Location to, Piece[][] b) {
 		boolean tf = true;
-		if (b[to.getRow()][to.getColumn()] == null && b[to.getRow()][to.getColumn()].getPlayer() != getPlayer())
+		if (b[to.getRow()][to.getColumn()] == null || b[to.getRow()][to.getColumn()].getPlayer() != getPlayer())
 			tf = true;
 		else {
 			tf = false;
 		}
 		for (int row = 0; row < Math.abs(to.getRow() - from.getRow()); row++) {
 			for (int column = 0; row < Math.abs(to.getColumn() - from.getColumn()); column++) {
-				if (row == column && b[from.getRow() + row][from.getColumn() + column] != null) {
+				if (row != column || b[from.getRow() + row][from.getColumn() + column] != null) {
 					tf = false;
 				}
 			}
